7c452afe442f629aa078e468ea97216584fac36f
Nylux <nylux42@gmail.com>
2023-12-29 02:46:39 +0100

    Initial commit

---
+++ Quicksave.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ModShardLauncher;
+using ModShardLauncher.Mods;
+
+namespace Quicksave;
+
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+public class Quicksave : Mod
+{
+    public override string Name => "Quicksave";
+    public override string Author => "Nylux";
+    public override string Description => "Allows the player to quicksave the game by pressing F5.";
+    public override string Version => "1.0.0";
+
+    public override void PatchMod()
+    {
+        ModLoader.InsertDecompiledCode("scr_smoothSaveAuto()", "gml_Object_o_player_KeyPress_116", 0);
+    }
+}
